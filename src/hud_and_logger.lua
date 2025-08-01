4c0837efeb8b27147bffe807b163514c793ea5bb
Chris Brophy <61714834+Chris-B33@users.noreply.github.com>
2025-08-01 04:51:15 +0100

    Update hud_and_logger.lua

--- src/hud_and_logger.lua
+++ src/hud_and_logger.lua
@@ -5,13 +5,13 @@ local function drawHUD(stats)
 		gui.text(5, 595, "No racer data")
 		return
 	end
-
+	
 	gui.text(5, 565, "Speed: " .. stats.speed)
 	gui.text(5, 580, string.format("Pos: x=%d y=%d z=%d", stats.pos[1], stats.pos[2], stats.pos[3]))
 	gui.text(5, 595, "Angle: " .. stats.drift_angle)
 
-    gui.text(5, 550, string.format("Grounded: %s, framesInAir=%d", stats.isGrounded, stats.framesInAir))
-    gui.text(5, 535, string.format("Going Backwards: %s", stats.isGoingBackwards))
+	gui.text(5, 550, string.format("Grounded: %s, framesInAir=%d", stats.isGrounded, stats.framesInAir))
+	gui.text(5, 535, string.format("Going Backwards: %s", stats.isGoingBackwards))
 end
 
 local function writeStatsAndCtrls(stats, ctrls)
